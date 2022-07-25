148fdc7d47c24e26a369ccf6e417a1114a888231
fraolb <Fraolbereket@gmail.com>
2022-07-25 23:08:17 +0300

    The main body

---
+++ src/Feed.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './Feed.css'
+import TweetBox from './TweetBox'
+import Post from './Post'
+
+const Feed = () => {
+  return (
+    <div className='feed'>
+         {/* Header */}       
+        <div className='feed__header'>
+         <h2>Home</h2>
+        </div>
+       
+        {/* TweetBox */}
+        <TweetBox />
+
+        {/* Post */}
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+
+        {/* Post */}
+        {/* Post */}
+        {/* Post */}
+        {/* Post */}
+        {/* Post */}
+        {/* Post */}
+        {/* Post */}
+    </div>
+  )
+}
+
+export default Feed
